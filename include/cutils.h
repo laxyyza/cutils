422ab8183c5da390505e72fbf90d669c33d74854
laxyyza <laxyy420@proton.me>
2024-11-03 09:36:21 +0200

    Add clampi() and clampf()

--- include/cutils.h
+++ include/cutils.h
@@ -4,5 +4,20 @@
 #include "int.h"
 #include "array.h"
 #include "mmframes.h"
+#include <math.h>
+
+static inline i32 
+clampi(i32 val, i32 min, i32 max)
+{
+	if (val < min) return min;
+	if (val > max) return max;
+	return val;
+}
+
+static inline f32
+clampf(f32 val, f32 min, f32 max)
+{
+	return fmaxf(min, fminf(val, max));
+}
 
 #endif // _CUTILS_H_
